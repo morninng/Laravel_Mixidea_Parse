243dd816e1efbc84a63bddd0a102a38761cb9a29
morninng <mr_morninng_star_dash@yahoo.co.jp>
2015-06-02 01:02:39 +0900

    cancel button works on parse cloud

--- cloud/main.js
+++ cloud/main.js
@@ -3,6 +3,117 @@ Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
 
+Parse.Cloud.define("CancelGame", function(request, response) {
+  console.log("cancel game called");
+  var self = this;
+  self.game_id = request.params.game_id;
+  self.game_obj = new Object();
+  self.user = request.user;
+  self.user_id = self.user.id;
+  self.role_name = request.params.role;
+  self.parent_event_obj = new Object();
+  console.log("user id is" + self.user_id );
+  console.log("game id is" + self.game_id);
+  console.log("role name is" + self.role_name);
+
+  var error_response = {code:1, message: "default_message", game_obj: self.game_obj};
+  // 1: default message
+  // 3: game do not exist
+  // 4: event do not exist
+  // 5: failure to save game data
+  // 6: contradiction situation to cancel the role
+  var Game = Parse.Object.extend("Game");
+  var game_query = new Parse.Query(Game);
+  game_query.get(self.game_id, {
+    success: function(game_obj){
+      var attendance = null;
+      self.game_obj = game_obj;
+      console.log("game obj is" + self.game_obj.id);
+      self.parent_event_obj_id  = self.game_obj.get("parent_event");
+      console.log("parent event obj is " + self.parent_event_obj_id);
+      var participant_obj = self.game_obj.get("participant_role");
+      console.log("participant fole obj is " + participant_obj)
+      if(participant_obj){
+        attendance = participant_obj[self.role_name];
+        console.log("attendance is " + attendance);
+        if(attendance && attendance==self.user_id){
+
+          delete participant_obj[self.role_name];
+          self.game_obj.set("participant_role", participant_obj);
+          self.game_obj.save().then(function(){
+            decrement_event_participant(request, response, self.parent_event_obj_id, self.user, self.game_obj);
+          },function(error){
+            error_response = {code:5, message: "failure to save game data", game_obj: self.game_obj};
+            response.error(error_response);
+          });
+        }else{
+          error_response = {code:6, message: "contradiction situation to cancel the role", game_obj: self.game_obj};
+          response.error(error_response);
+        }
+      }
+    },
+    error: function(error){
+      error_response = {code:3, message: "game do not exist", game_obj: self.game_obj};
+      response.error(error_response);
+    }
+  });
+});
+
+
+function decrement_event_participant(request, response, event_id, user, game_obj){
+  console.log(" event participant data remove");
+
+  var self = this;
+  self.game_obj = game_obj;
+  self.user = user;
+  self.user_id = user_id;
+
+  var Event = Parse.Object.extend("Event");
+  var event_query = new Parse.Query(Event);
+  console.log(event_id);
+
+  event_query.get(event_id, {
+    success: function(event_obj){
+      console.log("event retrieve success");
+
+      var participant_array = null;
+      var participated = true;
+      participant_array = event_obj.get("participants");
+      if(participant_array){
+        console.log("user id is" + self.user_id);
+        for(var key in participant_array ){
+          console.log("registered key is" + key);
+          if(key == self.user_id){
+            participant_array[key]--;
+            if(participant_array[key] < 1){
+              delete participant_array.key;
+              var game_participate = self.user.relation("game_participate");
+              game_participate.remove(self.game_obj);
+              var event_participate = self.user.relation("event_participate");
+              event_participate.remove(event_obj);
+            }
+          }
+        }
+      }
+      event_obj.set("participants", participant_array);
+      event_obj.save().then(function(obj){
+        return self.user.save();
+      }).then(function(obj){
+        console.log("saving user relation success");
+        response.success(self.game_obj);
+      },function(error){
+        console.log("saving game is failed or saving user data failed");
+        error_response = {code:5, message: "failure to save game data", game_obj: self.game_obj};
+        response.error(error_response);
+      });
+    },
+    error: function(object, error) {
+      error_response = {code:4, message: "event do not exist", game_obj: game_obj_str};
+      response.error(error_response_str);
+    }
+  });
+};
+
 Parse.Cloud.define("JoinGame", function(request, response) {
 
   console.log("join game called");
@@ -21,8 +132,6 @@ Parse.Cloud.define("JoinGame", function(request, response) {
   // 3: game do not exist
   // 4: event do not exist
   // 5: failure to save game data
-
-
 
   var Game = Parse.Object.extend("Game");
   var game_query = new Parse.Query(Game);
@@ -82,7 +191,6 @@ function increment_event_participant(request, response, event_id, user, game_obj
     success: function(event_obj){
       console.log("event retrieve success");
 
-
       var participant_array = null;
       var already_participated = false;
     	participant_array = event_obj.get("participants");
@@ -124,9 +232,7 @@ function increment_event_participant(request, response, event_id, user, game_obj
     },
     error: function(object, error) {
       console.log("retrieving event object failed");
-      var game_obj_str = JSON.stringify(self.game_obj);
       error_response = {code:4, message: "event do not exist", game_obj: game_obj_str};
-      var error_response_str = JSON.stringify(error_response);
       response.error(error_response_str);
     }
   });
