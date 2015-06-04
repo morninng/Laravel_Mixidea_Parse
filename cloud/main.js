3ca6f3425b719b578bb2098a48a9601b7c57ed17
morninng <mr_morninng_star_dash@yahoo.co.jp>
2015-06-05 00:20:20 +0900

    data structure of event game hierarchy is corrected

--- cloud/main.js
+++ cloud/main.js
@@ -60,12 +60,14 @@ function Create_Event(request, response, event_JSON){
 
 Create_Event.prototype.create_round_game = function(request, response,i){
 
-  console.log("create round game is called");
+  console.log("create round game is called i=" +i);
   var self = this;
 
   var Game = Parse.Object.extend("Game");
   var mixidea_game = new Game();
   mixidea_game.set("style", self.param.game_obj_array[i].style);
+  mixidea_game.set("type", self.param.event_type);
+  mixidea_game.set("gernre", self.param.game_obj_array[i].genre);
   mixidea_game.set("motion", self.param.game_obj_array[i].motion);
 //  mixidea_game.set("date_time", self.param.event_date);
   mixidea_game.set('parent_event', self.event_object.id);
@@ -84,12 +86,13 @@ Create_Event.prototype.create_round_game = function(request, response,i){
     return mixidea_round.save();
   }).then(function(round_obj){
     self.round_object_array[i] = round_obj;
-    console.log( i + "th round has been created");
+    console.log( i + "th round has been created :" + self.round_object_array[i]);
     console.log("number of game is " + self.param.game_obj_array.length);
 
     self.count.number_of_round_created++;
-    if(self.count.number_of_round_created == self.param.game_obj_array.length -1){
-      self.create_event_hierarchy(request, response);
+    if(self.count.number_of_round_created == self.param.game_obj_array.length){
+      console.log("i when create event hierararchy is called is" + i);
+      self.create_event_hierarchy(request, response, i);
     }
 
   }), function(error) {
@@ -99,15 +102,17 @@ Create_Event.prototype.create_round_game = function(request, response,i){
 }
 
 
-Create_Event.prototype.create_event_hierarchy = function(request, response){
+Create_Event.prototype.create_event_hierarchy = function(request, response, n){
 
     var self = this;
-    console.log("create_event_hierarchy is called")
+    console.log("create_event_hierarchy is called, number of round is" + n);
     var round_obj_for_hierarchy_array = new Object();
     var event_obj_for_hierarchy = new Object();
+    var round_obj_array = new Array();
+    var game_obj_array = new Array();
 
     for(var i=0; i<self.round_object_array.length; i++){
-
+      console.log("round obj length is " + self.round_object_array.length);
       var game_obj_id = self.game_object_array[i].id;
       var str_game_style = self.game_object_array[i].get("style");
       console.log("str_game_style is " +str_game_style);
@@ -115,15 +120,21 @@ Create_Event.prototype.create_event_hierarchy = function(request, response){
       game_obj_for_hierarchy = {game_ID: game_obj_id, style: str_game_style};
       var game_obj_for_hierarchy_array = new Array();
       game_obj_for_hierarchy_array.push(game_obj_for_hierarchy);
+      game_obj_array.push(game_obj_id);
+      console.log("game obj id is" + game_obj_id);
 
       var round_obj_for_hierarchy = new Object();
       round_obj_for_hierarchy = {round_ID: self.round_object_array[i].id , game_array: game_obj_for_hierarchy_array};
       round_obj_for_hierarchy_array = new Array();
+      round_obj_array.push(self.round_object_array[i].id );
+      console.log("round obj id is" + self.round_object_array[i].id );
       round_obj_for_hierarchy_array.push(round_obj_for_hierarchy);
     }
 
     event_obj_for_hierarchy = {round_array : round_obj_for_hierarchy_array};
     self.event_object.set("event_hierarchy", event_obj_for_hierarchy);
+    self.event_object.set("round", round_obj_array);
+    self.event_object.set("game", game_obj_array);
 
     self.event_object.save().then(function(event_obj){
       self.event_object = event_obj;
