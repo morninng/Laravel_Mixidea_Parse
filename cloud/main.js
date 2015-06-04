5bf0eef5a6a4e4f2eee160c2a3c94e4ae490d485
morninng <mr_morninng_star_dash@yahoo.co.jp>
2015-06-04 22:19:47 +0900

    event is saved on parse cloud

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,142 @@
 
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.define("Cloud_CreateEvent", function(request, response) {
+
+  var str_event_obj = request.params.event_obj;
+  var event_JSON = JSON.parse(str_event_obj);
+  new Create_Event(request, response, event_JSON);
+
 });
+
+// this event creation is for creating a online event where only one game exist in one round.
+//the tournament style of event creation function, multiple game exist in one round, should be implemented later.
+
+function Create_Event(request, response, event_JSON){
+
+  console.log("create event is called");
+  var self = this;
+  self.param = new Object();
+  self.param.event_title = event_JSON.event_title;
+  self.param.event_description = event_JSON.event_description;
+  self.param.event_date = event_JSON.event_date;
+  self.param.event_time = event_JSON.event_time;
+  self.param.event_type = event_JSON.event_type;
+  self.param.game_obj_array = event_JSON.game_object_array;
+  console.log(self.param.game_obj_array);
+  self.count = new Object();
+  self.count.number_of_round_created = 0;
+  self.event_object = new Object();
+  self.game_object_array = new Array();
+  self.round_object_array = new Array();
+
+  var Event = Parse.Object.extend("Event");
+  var mixidea_event = new Event();
+//  mixidea_event.set("date_time", self.param.event_date);
+  mixidea_event.set("title", self.param.event_title);
+  mixidea_event.set("type", self.param.event_type);
+  mixidea_event.set("description", self.param.event_description);
+  console.log("before save");
+  mixidea_event.save(null,{
+    success: function(obj) {
+      console.log("saving event success");
+
+      self.event_object = obj;
+      console.log("gambe obj array length is" + self.param.game_obj_array.length);
+      if(self.param.game_obj_array.length >1){
+        for(var i=0; i< self.param.game_obj_array.length; i++){
+          console.log("before calling create round game");
+          self.create_round_game(request, response,i);
+        }
+      }else{
+        error_response = {code:6, message:"game object is not defined"};
+        response.error(error_response);     
+      }
+    },
+    error: function(obj, error){
+      error_response = {code:6, message:"saving event data failed"};
+      response.error(error_response);
+    }
+  });
+}
+
+Create_Event.prototype.create_round_game = function(request, response,i){
+
+  console.log("create round game is called");
+  var self = this;
+
+  var Game = Parse.Object.extend("Game");
+  var mixidea_game = new Game();
+  mixidea_game.set("style", self.param.game_obj_array[i].style);
+  mixidea_game.set("motion", self.param.game_obj_array[i].motion);
+//  mixidea_game.set("date_time", self.param.event_date);
+  mixidea_game.set('parent_event', self.event_object.id);
+  console.log("before calling saving mixidea game ")
+
+  mixidea_game.save().then(function(game_obj){
+
+    console.log( " game has been created");
+    console.log( i + "th game has been created");
+    self.game_object_array[i] = game_obj;
+
+    var Round = Parse.Object.extend("Round");
+    var mixidea_round = new Round();  
+    mixidea_round.add("game",self.game_object_array[i].id);
+    mixidea_round.set('parent_event', self.event_object.id);
+    return mixidea_round.save();
+  }).then(function(round_obj){
+    self.round_object_array[i] = round_obj;
+    console.log( i + "th round has been created");
+    console.log("number of game is " + self.param.game_obj_array.length);
+
+    self.count.number_of_round_created++;
+    if(self.count.number_of_round_created == self.param.game_obj_array.length -1){
+      self.create_event_hierarchy(request, response);
+    }
+
+  }), function(error) {
+    error_response = {code:6, message:"saving game or round failed"};
+    response.error(error_response);
+  };
+}
+
+
+Create_Event.prototype.create_event_hierarchy = function(request, response){
+
+    var self = this;
+    console.log("create_event_hierarchy is called")
+    var round_obj_for_hierarchy_array = new Object();
+    var event_obj_for_hierarchy = new Object();
+
+    for(var i=0; i<self.round_object_array.length; i++){
+
+      var game_obj_id = self.game_object_array[i].id;
+      var str_game_style = self.game_object_array[i].get("style");
+      console.log("str_game_style is " +str_game_style);
+      var game_obj_for_hierarchy = new Object();
+      game_obj_for_hierarchy = {game_ID: game_obj_id, style: str_game_style};
+      var game_obj_for_hierarchy_array = new Array();
+      game_obj_for_hierarchy_array.push(game_obj_for_hierarchy);
+
+      var round_obj_for_hierarchy = new Object();
+      round_obj_for_hierarchy = {round_ID: self.round_object_array[i].id , game_array: game_obj_for_hierarchy_array};
+      round_obj_for_hierarchy_array = new Array();
+      round_obj_for_hierarchy_array.push(round_obj_for_hierarchy);
+    }
+
+    event_obj_for_hierarchy = {round_array : round_obj_for_hierarchy_array};
+    self.event_object.set("event_hierarchy", event_obj_for_hierarchy);
+
+    self.event_object.save().then(function(event_obj){
+      self.event_object = event_obj;
+      console.log("event object saved again");
+      response.success(event_obj);
+    }), function(error) {
+      console.log("error happen during setting event hierarchy" + error);
+    };
+
+}
+
+
+
 
 Parse.Cloud.define("CancelGame", function(request, response) {
   console.log("cancel game called");
